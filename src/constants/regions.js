dd66e15a31667576b6ed39f445d9eaf0a019b0f1
djirdehh <hassan.djirdeh@mail.utoronto.ca>
2017-04-14 22:08:58 -0400

    adding region data

---
+++ src/constants/regions.js
@@ -0,0 +1,12 @@
+exports.ALL = [
+	{ label: 'The North', value: 'the%20north' },
+	{ label: 'The Vale of Arryn', value: 'the%20vale' },
+	{ label: 'The Riverlands', value: 'the%20riverlands' },
+	{ label: 'The Westerlands', value: 'the%20westerlands' },
+	{ label: 'The Iron Islands', value: 'iron%20islands' },
+	{ label: 'The Crownlands', value: 'the%20crownlands' },
+	{ label: 'The Stormlands', value: 'the%20stormlands' },
+	{ label: 'The Reach', value: 'the%20reach' },
+	{ label: 'Dorne', value: 'dorne' },
+	{ label: 'Beyond The Wall', value: 'beyond%20the%20wall' }
+]
